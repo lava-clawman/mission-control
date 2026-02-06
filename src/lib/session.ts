04c672dcb9f1e0bc10c003086ea438140e901800
Lava <emcfi2024@gmail.com>
2026-02-06 22:08:01 +1300

    fix: remove all auth-related files for now

--- src/lib/session.ts
+++
@@ -1,8 +0,0 @@
-import { getIronSession } from 'iron-session'
-import { cookies } from 'next/headers'
-import { sessionOptions, SessionData } from './auth-config'
-
-export async function getSession() {
-  const cookieStore = await cookies()
-  return getIronSession<SessionData>(cookieStore, sessionOptions)
-}
